ea83f8b7900c0447ae7319d5fd8af0c3d01c26af
David Whiteside <starboarder2001@gmail.com>
2016-06-11 06:54:38 -0600

    improved testing for first parser statement

--- test/units/test_cli_cli.py
+++ test/units/test_cli_cli.py
@@ -1,13 +1,20 @@
 import nose
-from outbit.cli.cli import Cli
+from outbit.cli import cli as cli
 import unittest
 import sys
 import os
 
 class TestCli(unittest.TestCase):
+    def test_p_action_run(self):
+        t = []
+        t.append(None)
+        t.append(["help",])
+        cli.p_action_run(t)
+        assert(cli.parser_category == "/" and cli.parser_action == "help")
+
     def test_project(self):
         # Test No Path Given (Hello Test)
         sys.argv[1] = ""
         sys.argv[2] = ""
-        cli = Cli()
-        assert(cli is not None)
+        cliobj = cli.Cli()
+        assert(cliobj is not None)
