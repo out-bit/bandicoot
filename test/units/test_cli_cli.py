2d2ff3ec65b5fee6dc1ea9efacc1f2b039e564d5
David Whiteside <starboarder2001@gmail.com>
2016-06-11 06:59:55 -0600

    added unit tests for p_action_run

--- test/units/test_cli_cli.py
+++ test/units/test_cli_cli.py
@@ -5,12 +5,28 @@ import sys
 import os
 
 class TestCli(unittest.TestCase):
-    def test_p_action_run(self):
+    def test_p_action_run_help(self):
         t = []
         t.append(None)
         t.append(["help",])
         cli.p_action_run(t)
         assert(cli.parser_category == "/" and cli.parser_action == "help")
+
+    def test_p_action_run_users_list(self):
+        t = []
+        t.append(None)
+        t.append(["users","list"])
+        cli.p_action_run(t)
+        assert(cli.parser_category == "/users" and cli.parser_action == "list")
+
+    def test_p_action_run_users_del(self):
+        t = []
+        t.append(None)
+        t.append(["users","del"])
+        t.append(" ")
+        t.append({"username": "jdoe"})
+        cli.p_action_run(t)
+        assert(cli.parser_category == "/users" and cli.parser_action == "del" and "username" in cli.parser_options)
 
     def test_project(self):
         # Test No Path Given (Hello Test)
