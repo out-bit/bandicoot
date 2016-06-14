c007fc31713f21a02c81fede7eb63253b148efbd
David Whiteside <starboarder2001@gmail.com>
2016-06-13 20:48:22 -0600

    fixed #25. syntax for actions=/ works now

--- lib/outbit/parser/lex.py
+++ lib/outbit/parser/lex.py
@@ -6,7 +6,7 @@ tokens = ("ACTION", "OPTIONVAL", "OPTIONVALS", "OPTIONVALD", "SPACE", "EQUAL")
 t_ACTION        =r'[a-zA-Z0-9_\-]+'
 t_OPTIONVALS    =r"'[,\"a-zA-Z0-9_/\s\-]+'"
 t_OPTIONVALD    =r'"[,\'a-zA-Z0-9_/\s\-]+"'
-t_OPTIONVAL     =r'/[,a-zA-Z0-9_/\-]+'
+t_OPTIONVAL     =r'/[,a-zA-Z0-9_/\-]*'
 t_SPACE         =r'\s+'
 t_EQUAL         =r'='
 
