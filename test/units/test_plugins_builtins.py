809ac99879072f7e23b8383678cbb3182e08fc4a
David Whiteside <starboarder2001@gmail.com>
2016-06-13 20:51:17 -0600

    fixed unit tests after adding username as an argument to a plugin

--- test/units/test_plugins_builtins.py
+++ test/units/test_plugins_builtins.py
@@ -11,47 +11,47 @@ class TestCli(unittest.TestCase):
     def test_plugin_help(self, mock_test):
         tester = mock.Mock()
         mock_test.return_value = tester
-        result = builtins.plugin_help(None, None)
-        assert("users list" in result)
+        result = builtins.plugin_help(None, None, None)
+        assert("exit" in result)
 
     def test_plugin_ping(self):
-        result = builtins.plugin_ping(None, None)
+        result = builtins.plugin_ping(None, None, None)
         assert(result == json.dumps({"response": "  pong"}))
 
     def test_plugin_users_del(self):
-        result = builtins.plugin_users_del(None, {})
+        result = builtins.plugin_users_del(None, None, {})
         assert(result == json.dumps({"response": "  name option is required"}))
 
     def test_plugin_users_add(self):
-        result_uonly = builtins.plugin_users_add(None, {"username": "jdoe"})
-        result_ponly = builtins.plugin_users_add(None, {"password": "jdoe"})
+        result_uonly = builtins.plugin_users_add(None, None, {"username": "jdoe"})
+        result_ponly = builtins.plugin_users_add(None, None, {"password": "jdoe"})
         assert(result_uonly == json.dumps({"response": "  username and password are required options"})
             and result_ponly == json.dumps({"response": "  username and password are required options"}))
 
     def test_plugin_actions_add_name_missing(self):
-        result = builtins.plugin_actions_add(None, {"category": "/"})
+        result = builtins.plugin_actions_add(None, None, {"category": "/"})
         assert(result == json.dumps({"response": "  name option is required"}))
 
     def test_plugin_actions_add_category_missing(self):
-        result = builtins.plugin_actions_add(None, {"name": "jaction"})
+        result = builtins.plugin_actions_add(None, None, {"name": "jaction"})
         assert(result == json.dumps({"response": "  category option is required"}))
 
     def test_plugin_actions_add_action_missing(self):
-        result = builtins.plugin_actions_add(None, {"name": "jaction", "category": "/"})
+        result = builtins.plugin_actions_add(None, None, {"name": "jaction", "category": "/"})
         assert(result == json.dumps({"response": "  action option is required"}))
 
     def test_plugin_actions_add_plugin_missing(self):
-        result = builtins.plugin_actions_add(None, {"name": "jaction", "category": "/", "action": "test"})
+        result = builtins.plugin_actions_add(None, None, {"name": "jaction", "category": "/", "action": "test"})
         assert(result == json.dumps({"response": "  plugin option is required"}))
 
     def test_plugin_actions_del(self):
-        result = builtins.plugin_actions_del(None, {})
+        result = builtins.plugin_actions_del(None, None, {})
         assert(result == json.dumps({"response": "  name option is required"}))
 
     def test_plugin_roles_add(self):
-        result = builtins.plugin_roles_add(None, {})
+        result = builtins.plugin_roles_add(None, None, {})
         assert(result == json.dumps({"response": "  name option is required"}))
 
     def test_plugin_roles_del(self):
-        result = builtins.plugin_roles_del(None, {})
+        result = builtins.plugin_roles_del(None, None, {})
         assert(result == json.dumps({"response": "  name option is required"}))
