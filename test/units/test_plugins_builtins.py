421d7936ea8acdeba76eefd1e44c4f2b149fee3c
David Whiteside <starboarder2001@gmail.com>
2016-06-16 20:23:58 -0600

    fixed mongomock issue

--- test/units/test_plugins_builtins.py
+++ test/units/test_plugins_builtins.py
@@ -9,10 +9,14 @@ import outbit.cli.api
 
 
 class TestCli(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        self.mock_db_setup()
+        self.mock_db_basic_database()
+        unittest.TestCase.__init__(self, *args, **kwargs)
+
     def mock_db_setup(self):
-        if outbit.cli.api.dbclient is None and outbit.cli.api.db is None:
-            outbit.cli.api.dbclient = mongomock.Connection()
-            outbit.cli.api.db = outbit.cli.api.dbclient.conn["outbit"]
+        outbit.cli.api.dbclient = mongomock.MongoClient()
+        outbit.cli.api.db = outbit.cli.api.dbclient.conn["outbit"]
 
     def mock_db_basic_database(self):
         outbit.cli.api.db.users.insert_one({"username": "jdoe1", "password_md5": "md5test"})
@@ -28,14 +32,10 @@ class TestCli(unittest.TestCase):
 
     @mock.patch("outbit.cli.api.db")
     def test_plugin_help(self, mock_test):
-        self.mock_db_setup()
-        self.mock_db_basic_database()
         result = builtins.plugin_help("jdoe3", {}, {})
         assert("exit" in result)
 
     def test_plugin_ping(self):
-        self.mock_db_setup()
-        self.mock_db_basic_database()
         result = builtins.plugin_ping("jdoe3", {}, {})
         assert(result == json.dumps({"response": "  pong"}))
 
@@ -43,11 +43,22 @@ class TestCli(unittest.TestCase):
         result = builtins.plugin_users_del("jdoe3", {}, {})
         assert(result == json.dumps({"response": "  username option is required"}))
 
+    def test_plugin_users_del_does_not_exist(self):
+        result = builtins.plugin_users_del("jdoe3", {}, {"username": "user_nomatch"})
+        assert(result == json.dumps({"response": "  user user_nomatch does not exist"}))
+
     def test_plugin_users_del(self):
-        self.mock_db_setup()
-        self.mock_db_basic_database()
         result = builtins.plugin_users_del("jdoe3", {}, {"username": "jdoe1"})
         assert(result == json.dumps({"response": "  deleted user jdoe1"}))
+
+    def test_plugin_users_add(self):
+        result = builtins.plugin_users_add("jdoe3", None, {"username": "add_test", "password": "a"})
+        print(result)
+        assert (result == json.dumps({"response": "  created user add_test"}))
+
+    def test_plugin_users_add_already_exists(self):
+        result = builtins.plugin_users_add(None, None, {"username": "jdoe1", "password": "a"})
+        assert (result == json.dumps({"response": "  user jdoe1 already exists"}))
 
     def test_plugin_users_add_name_missing(self):
         result_uonly = builtins.plugin_users_add(None, None, {"username": "jdoe"})
